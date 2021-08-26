7c15c57758934344b6f78411ac0c7aeb9ae1efb2
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-26 09:59:39 +0530

    registration view created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import CreateUserSerializer , UserSerializer
+from rest_framework import viewsets , permissions , generics
 
 # Create your views here.
+
+
+class RegistrationAPI(generics.GenericAPIView):
+    serializer_class  = CreateUserSerializer
+
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_excpetion=True)
+        user = serializer.save()
+
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data,
+            "token":AuthToken.objects.create(user)[1]
+        })
