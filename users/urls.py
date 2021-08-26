0e031fde29a2cacb9b816d91ccb7963477ec6c5e
krishnajhapate <ki54.jhapate@gmail.com>
2021-08-26 10:49:56 +0530

    create login view and endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegistrationAPI
+from .views import RegistrationAPI,LoginAPI
 
 urlpatterns = [
-    path('registration/',RegistrationAPI.as_view(),name="registration")
+    path('registration/',RegistrationAPI.as_view(),name="registration"),
+    path('Login/',LoginAPI.as_view(),name="login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import CreateUserSerializer , UserSerializer
+from .serializers import CreateUserSerializer , UserSerializer,LoginUserSerializer
 from rest_framework import viewsets , permissions , generics
 
 # Create your views here.
@@ -12,9 +12,21 @@ class RegistrationAPI(generics.GenericAPIView):
 
     def post(self,request,*args,**kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_excpetion=True)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
 
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data,
+            "token":AuthToken.objects.create(user)[1]
+        })
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validate_data
         return Response({
             "user":UserSerializer(user,context=self.get_serializer_context()).data,
             "token":AuthToken.objects.create(user)[1]
